ae3af4847e3358f86ecfaf6a53fa361890716413
Brian0317 <onedev975@gmail.com>
2023-02-22 23:06:22 +0300

    1

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -154,7 +154,7 @@ contract Staking is
     if (currentRatio > stakedRatio) {
       rewards = (sharesToWithdraw * (currentRatio - stakedRatio)) / base;
     }
-//calculate 
+//calculate  gfgfg
     stakeholderToStake[msg.sender].shares -= sharesToWithdraw;
     stakeholderToStake[msg.sender].stakedSTK -= stakeAmount;
     totalStakes -= stakeAmount;
